99282cad5e51b9fca6a439a9c0d913e46096ff01
lclrisoli <citizenlambda@laposte.net>
2022-01-20 19:07:13 +0100

    Version 1.3 - Attribute not allowed in HTML5, New browsers version

--- src/js/bandeau.js
+++ src/js/bandeau.js
@@ -1,4 +1,4 @@
 
 function afficherBandeau(){
-    this.document.write("<table summary='Logo Free' width='100%'><tr><td><a href='http://www.free.fr'><img alt='Logo Free' src='./png/logofree.png' width='220' height='48' border='0'></a>&nbsp;</td></tr></table>");
+    this.document.write("<table style='width: 100%'><tr><td><a href='http://www.free.fr'><img alt='Logo Free' src='./png/logofree.png' width='220' height='48' style='border: 0px'></a>&nbsp;</td></tr></table>");
 }
